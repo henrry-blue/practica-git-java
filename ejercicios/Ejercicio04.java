ae7fdbbd06be0d347111bb56b572eabfe3ba5c27
henrry-blue <trollino465@gmail.com>
2026-09-14 16:23:31 -0400

    feat: add exercise 04

---
+++ ejercicios/Ejercicio04.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese un número: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("El número " + numero + " es par.");
+        } else {
+            System.out.println("El número " + numero + " es impar.");
+        }
+        
+        scanner.close();
+    }
+}
