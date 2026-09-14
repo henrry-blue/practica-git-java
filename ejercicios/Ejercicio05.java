8fe0e4938f7be2baf0629dc36bcdeae6cde0db04
henrry-blue <trollino465@gmail.com>
2026-09-14 16:26:56 -0400

    feat: add exercise 05

---
+++ ejercicios/Ejercicio05.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese A: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Ingrese B: ");
+        int b = scanner.nextInt();
+        
+        System.out.print("Ingrese C: ");
+        int c = scanner.nextInt();
+        
+        int mayor;
+        
+        if (a >= b && a >= c) {
+            mayor = a;
+        } else if (b >= a && b >= c) {
+            mayor = b;
+        } else {
+            mayor = c;
+        }
+        
+        System.out.println("El número mayor es: " + mayor);
+        
+        scanner.close(); 
+    }
+}
