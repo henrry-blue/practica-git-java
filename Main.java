d29a8c3d3319698228ec00bcbe416770d1a8228a
henrry-blue <trollino465@gmail.com>
2026-09-14 16:05:13 -0400

    feat: add project introduction

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
-
+ 
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
+        System.out.println("Mi primer proyecto con Git y Java");
     }
 }
