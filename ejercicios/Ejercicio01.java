3b798589df22af7a1fe5cbf9e3d79a914abfa5f0
henrry-blue <trollino465@gmail.com>
2026-09-14 16:12:57 -0400

    feat: add exercise 01

---
+++ ejercicios/Ejercicio01.java
@@ -0,0 +1,15 @@
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        // Declaración de variables
+        String nombre = "Juan Pérez";
+        int edad = 20;
+        String carrera = "Ingeniería de Sistemas";
+        String universidad = "UATF";
+
+        // Impresión de resultados en consola
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Carrera: " + carrera);
+        System.out.println("Universidad: " + universidad);
+    }
+}
