abed080a5987f1ad08a2e332f8d980f1cfe9fb14
henrry-blue <trollino465@gmail.com>
2026-09-14 16:31:07 -0400

    feat: add readme and update exercise 02

---
+++ ejercicios/Ejercicio02.java
@@ -0,0 +1,21 @@
+public class Ejercicio02 {
+    public static void main(String[] args) {
+        // Definir los números base
+        int a = 20;
+        int b = 5;
+
+        // Realizar los cálculos usando los operadores aritméticos
+        int suma = a + b;
+        int resta = a - b;
+        int multiplicacion = a * b;
+        int division = a / b;
+        int modulo = a % b;
+
+        // Mostrar las salidas esperadas por consola
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Módulo: " + modulo);
+    }
+}
